4e1e24a5498ac2936979dbfb7b8438509a20329e
technologyadeel <crewraj11@gmail.com>
2024-01-09 11:24:00 +0530

    Add Morgan for logging.

--- server/app.js
+++ server/app.js
@@ -1,5 +1,29 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { config } from "dotenv";
+import morgan from "morgan";
+config();
 
 const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true
+}));
+
+app.use(cookieParser())
+
+app.use(morgan('dev'))
+
+app.use('/ping', (req, res) => {
+    res.send('Pong')
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('OOPS!! 404 page not found');
+})
 
 export default app;
