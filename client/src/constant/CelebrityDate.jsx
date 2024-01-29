eff43bb0f0f0d84200b0784e9e98f5064e4907fb
technologyadeel <crewraj11@gmail.com>
2024-01-29 21:14:36 +0530

    cleaned up our about us page.

---
+++ client/src/constant/CelebrityDate.jsx
@@ -0,0 +1,38 @@
+import apj from "../assets/apj.png"
+import billGates from "../assets/billGates.png"
+import einstein from "../assets/einstein.png"
+import nelsonMandela from "../assets/nelsonMandela.png"
+import steveJobs from "../assets/steveJobs.png"
+
+export const celebrities = [
+    {
+      title: "Apj Abdul Kalam",
+      desc: "Determination is the power that sees us through all our frustration and obstacles.",
+      image: apj,
+      slideNumber: 1
+    },
+    {
+      title: "Nelson Mandela",
+      desc: "Education is the most powerful tool you use to change the world.",
+      image: nelsonMandela,
+      slideNumber: 2
+    },
+    {
+      title: "Bill Gates",
+      desc: "It's fine to celebrate success but it is more important to heed the lessons of failure.",
+      image: billGates,
+      slideNumber: 3
+    },
+    {
+      title: "Albert Eintein",
+      desc: "Anyone who has never made a mistake has never tried anything new.",
+      image: einstein,
+      slideNumber: 4
+    },
+    {
+      title: "Steve Jobs",
+      desc: "The most important decisions you make are not the things you do, but the things you decide not to do.",
+      image: steveJobs,
+      slideNumber: 5
+    },
+  ]
