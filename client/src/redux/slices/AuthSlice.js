28d20f34f0fab9564145fbd7d9d16c137ffcf290
technologyadeel <crewraj11@gmail.com>
2024-01-31 19:41:47 +0530

    fix typo in authSlice.

--- client/src/redux/slices/AuthSlice.js
+++ client/src/redux/slices/AuthSlice.js
@@ -19,7 +19,7 @@ export const createAccount = createAsyncThunk("auth/signup", async(data) => {
             error: "Failed to create account"
         });
         
-        return (await res).date
+        return (await res).data
     } catch (error) {
        toast.error(error?.response?.data?.message) 
     }
