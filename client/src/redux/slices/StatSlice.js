3791c03f1e5131cfff896168d491cb58dc1ccaa9
technologyadeel <crewraj11@gmail.com>
2024-02-27 10:59:56 +0530

    Added statSlice for admin.

---
+++ client/src/redux/slices/StatSlice.js
@@ -0,0 +1,33 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import toast from "react-hot-toast";
+import axiosInstance from "../../helpers/AxiosInstance";
+
+const initialState = {
+    allUserCount: 0,
+    subscribedCount: 0
+}
+
+export const getStatData = createAsyncThunk("stats/get", async () => {
+    try {
+        const response = axiosInstance.get("/admin/stats/users");
+        toast.promise(response, {
+            loading: "Getting the stats...",
+            success: (data) => {
+                return data?.data?.message
+            },
+            error: "Failed to load stats"
+        });
+        return (await response).data;
+    } catch (error) {
+        toast.error(error?.response?.data?.message)
+    }
+})
+
+const statSlice = createSlice({
+    name: "state",
+    initialState,
+    reducers: {},
+    extraReducers: () => {}
+});
+
+export default statSlice.reducer;
