2fa7cb57f5d35eb9e8f4724390df12e0a035a7ba
technologyadeel <crewraj11@gmail.com>
2024-11-04 01:20:32 +0530

    bug fix

--- client/src/helpers/AxiosInstance.js
+++ client/src/helpers/AxiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://learning-management-system-1-pcf6.onrender.com/api/v1';
+const BASE_URL = 'https://learning-management-system-2-xhu6.onrender.com/api/v1';
 
 const axiosInstance = axios.create();
 
